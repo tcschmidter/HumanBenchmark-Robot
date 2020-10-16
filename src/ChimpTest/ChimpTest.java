04ea2119a5ca315c57c12d2fc76edf292b878424
tcschmidter <70390725+tcschmidter@users.noreply.github.com>
2020-10-15 23:32:58 -0400

    Improved readability

--- src/ChimpTest/ChimpTest.java
+++ src/ChimpTest/ChimpTest.java
@@ -4,7 +4,6 @@ import java.awt.image.*;
 /**
  * ChimpTest
  *
- * Program completes the Are You Smarter Than a Chimpanzee test at https://humanbenchmark.com/tests/chimp
  * Program completes the Are You Smarter Than a 
  * Chimpanzee test at https://humanbenchmark.com/tests/chimp
  *
@@ -14,31 +13,33 @@ import java.awt.image.*;
 public class ChimpTest {
 
 	public static void main(String[]args) {
-		try {
-			getScreen();
-		}
-		catch(AWTException e) {
+		ChimpTestScreen screen = new ChimpTestScreen(getScreen());
+		
 
-		}
+
 	
 	}
 	//scans image and returns an array of pixels
-	public static String[][] getScreen() throws AWTException {
-		Robot robot = new Robot();
-		Rectangle rect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-		BufferedImage screen = robot.createScreenCapture(rect);
-		String[][]screenRGB = new String[screen.getWidth()][screen.getHeight()];
-		for (int y = 0; y < screen.getHeight(); y++) {
-		    for (int x = 0; x < screen.getWidth(); x++) {
-		        int pixel = screen.getRGB(x, y);
-		        String red = String.format("%03d", pixel >> 16 & 0xff);
-		        String green = String.format("%03d", pixel >> 8 & 0xff);
-		        String blue = String.format("%03d", pixel & 0xff);
-		        screenRGB[x][y] = red + green + blue; 
-	        }
-		                                                                                                                                                     
-	    }
-		return screenRGB;  
+	public static String[][] getScreen() {
+		try {
+			Robot robot = new Robot();
+			Rectangle rect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			BufferedImage screen = robot.createScreenCapture(rect);
+			String[][]screenRGB = new String[screen.getWidth()][screen.getHeight()];
+			for (int y = 0; y < screen.getHeight(); y++) {
+			    for (int x = 0; x < screen.getWidth(); x++) {
+			        int pixel = screen.getRGB(x, y);
+			        String red = String.format("%03d", pixel >> 16 & 0xff);
+			        String green = String.format("%03d", pixel >> 8 & 0xff);
+			        String blue = String.format("%03d", pixel & 0xff);
+			        screenRGB[x][y] = red + green + blue; 
+		        }
+			                                                                                                                                                     
+		    }
+			return screenRGB;  
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
